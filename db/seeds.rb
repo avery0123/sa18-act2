59db954d0d6d4d4de049a2871981e240e3c60bdd
Avery Oliver <averyaoliver03@gmail.com>
2024-04-22 21:35:14 -0500

    Added Product model, controller, views, routes, and seed data

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+# Create seed data for products
+Product.create(name: "Playstation 5", price: 499.99, description: "Sony's latest console")
+Product.create(name: "Xbox Series", price: 499.99, description: "Microsofts latest console")
+Product.create(name: "Nintendo Switch", price: 299.99, description: "Nintendo's latest console")
